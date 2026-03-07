0295e1e5642fad657d69ba8ad9ad50291ba93022
Benedikt Hofstätter <dev@bhofstaetter.de>
2026-03-07 03:36:54 +0100

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export {CollectionOperations} from '@/collection/CollectionOperations';
-export {CollectionRepository} from '@/collection/CollectionRepository';
-export {GlobalOperations} from '@/global/GlobalOperations';
-export {GlobalRepository} from '@/global/GlobalRepository';
+export {CollectionOperations} from '@/collection/CollectionOperations.js';
+export {CollectionRepository} from '@/collection/CollectionRepository.js';
+export {GlobalOperations} from '@/global/GlobalOperations.js';
+export {GlobalRepository} from '@/global/GlobalRepository.js';
 
 export type {AnyCollectionConfig, AnyGlobalConfig, DbId} from '@/types.js';
